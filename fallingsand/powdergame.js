92b7524ae71664e6378114cd03ac6b7b845c7283
troido <troido@hotmail.com>
2017-02-11 16:03:30 +0100

    added colours, density and you can create sand by clicking the mouse

--- fallingsand/powdergame.js
+++ fallingsand/powdergame.js
@@ -1,16 +1,20 @@
 
 const WIDTH = 500;
 const HEIGHT = 400;
-const SAND = 10000;
+const OBJECTS = 10000;
 const SIZE = WIDTH * HEIGHT;
 const DELAY = 30; // 1000/FPS
 
 // binary flags
-const SOLID = 1; // will probably be replaced by density
-const GRAVITY = 2;
+const DENSITY = 3;
+const LOW_DENSITY = 1;
+const MEDIUM_DENSITY = 2;
+const HIGH_DENSITY = 3;
+const GRAVITY = 8;
 const FLUID = 4;
 
-const DEFAULT_PARTICLE = SOLID | GRAVITY | FLUID;
+const SAND = HIGH_DENSITY | GRAVITY;
+const WATER = LOW_DENSITY | GRAVITY | FLUID;
 
 // declaring variables as local allows closure to simplify the names
 // remove these declarations after closure compiling
@@ -21,21 +25,24 @@ var field = []; // the main field holding all information
 // a list of all pacticles that can do somehing or ar drawn (static invisible walls can be excluded)
 var objects = [];
 
+var colours = [];
+colours[SAND] = "#ba8";
+colours[WATER] = "#22f";
 
 // add some random sand
-for (i=0; i<SAND;i++){
+for (i=0; i<OBJECTS;i++){
     // a particle consists of the pair of the binary flags and its position
-    objects[i] = [DEFAULT_PARTICLE, Math.random()*SIZE|0];
+    objects[i] = [WATER, Math.random()*SIZE|0];
     
     // make vertical wall
-    // it will have the height of SAND instead of HEIGHT, but as long as SAND>HEIGHT that's okay
-    field[i*WIDTH]=SOLID;
+    // it will have the height of OBJECTS instead of HEIGHT, but as long as OBJECTS>HEIGHT that's okay
+    field[i*WIDTH]=HIGH_DENSITY;
     // the floor can be done with less bytes in the move conditions
     // if there is only 1 moving down condition
 }
 
 
-setInterval(()=>{
+setInterval(_=>{
     
     
     c.clearRect(0,0,WIDTH, HEIGHT);
@@ -48,6 +55,7 @@ setInterval(()=>{
         
         // the drawing is one frame behind on the physics, but I don't think that matters
         // this is way simpler
+        c.fillStyle = colours[flags];
         c.fillRect(pos%WIDTH,pos/WIDTH|0,1,1);
         
         // if this cell has gravity
@@ -55,13 +63,16 @@ setInterval(()=>{
             // most probably go straight down, but there is a small chance to go left or right
             newPos = pos + WIDTH + (Math.random()*2.4-1.2|0);
             // if the newPos is not solid, or this cell is fluid and the cell above newPos is not solid
-            if(SOLID ^ field[newPos] & SOLID || flags & FLUID && SOLID ^ field[newPos -= WIDTH] & SOLID){
+            if((field[newPos] & DENSITY) < (flags & DENSITY) || flags & FLUID && (field[newPos -= WIDTH] & DENSITY) < (flags & DENSITY)){
                 // move to newPos
+                field[pos] = field[newPos];
                 field[newPos] = flags;
-                field[pos] = 0;
                 object[1] = newPos;
             }
         }
     }
     
 },DELAY);
+
+// when the mouse is pressed, create 20 objects under the cursor
+onmousedown = e => {for (i=20; i--;)objects.push([SAND, e.offsetX+e.offsetY*WIDTH])}
