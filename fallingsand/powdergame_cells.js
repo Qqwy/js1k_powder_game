efbc54b0157f80be7bddd56a0c7f3e8295f3c6ef
troido <troido@hotmail.com>
2017-02-14 17:12:36 +0100

    number keys select element; combined FLY and SLOW; renamed VISCOSITY to SPREAD and changed its function; made array for imageData colours because performance; removed VISIBLE bit; merged SPREAD and FLUID

--- fallingsand/powdergame_cells.js
+++ fallingsand/powdergame_cells.js
@@ -3,32 +3,33 @@ const WIDTH = 900;
 const HEIGHT = 600;
 const OBJECTS = 100000;
 const SIZE = WIDTH * HEIGHT;
-const DELAY = 15; // 1000/FPS
+const DELAY = 16; // 1000/FPS
 
-const DROP_SIZE = 5;
+const DROP_SIZE = 9;
 
 
 // binary flags
 
-const VISCOSITY = 3;
-// the lower the viscosity, the higher the chance of moving on the x axis
-// I'm still not sure whether the viscosity bits are worth their code
-const HIGH_VISCOSITY = 1;
-const MEDIUM_VISCOSITY = 2;
-const LOW_VISCOSITY = 3;
+// the higher the spread, the higher the chance of moving on the x axis
+// the values of these constants actually matter
+// since they are used in a multiplication
+const SPREAD = 6;
+const LOW_SPREAD = 2;
+const MEDIUM_SPREAD = 4;
+const HIGH_SPREAD = 6;
+// medium or high spread automatically implies fluid
+const FLUID = 4;
 
-const UPDATE_BIT = 4;
+const UPDATE_BIT = 1;
 const GRAVITY = 8;
-const FLUID = 16;
 const DENSITY = 192;
 const LOW_DENSITY = 64;
 const MEDIUM_DENSITY = 128;
 const HIGH_DENSITY = 192;
-const VISIBLE = 32;
 
-const FLY = 1024;
-const VOLATILE = 2048;
-const SLOW = 512;
+const VOLATILE = 32;
+const SLOW = 16;
+const FLY = SLOW;
 const TRAIL = 256;
 const REACT_ABOVE = 1<<12;
 const REACT_SIDE = 1<<13;
@@ -40,9 +41,9 @@ const REACT_NEIGHBOURS = REACT_ABOVE | REACT_SIDE | REACT_BELOW;
 const PRODUCT_SHIFT = 27;
 
 // those numbers refer to the index in particleTypes array
-const NUM_PLACABLE_TYPES = 7;
-const WOOD_PLACE = 0 + NUM_PLACABLE_TYPES;
+const NUM_PLACABLE_TYPES = 10;
 const FIRE_PLACE = 3;
+const WOOD_PLACE = 0 + NUM_PLACABLE_TYPES;
 const TREE_PLACE = 1 + NUM_PLACABLE_TYPES;
 const MUD_PLACE = 2 + NUM_PLACABLE_TYPES;
 
@@ -63,102 +64,98 @@ const WATERING = 1 << REAGENT_SHIFT + 2;
 
 
 // particle types
-const SAND = VISIBLE | HIGH_DENSITY | GRAVITY | HIGH_VISCOSITY | FLAMMABLE | SOIL | NEED_WATERING | (MUD_PLACE << PRODUCT_SHIFT);
-const MUD = VISIBLE | HIGH_DENSITY | GRAVITY | HIGH_VISCOSITY | SOIL | SLOW;
-const WATER = VISIBLE | MEDIUM_DENSITY | GRAVITY | FLUID | LOW_VISCOSITY | WATERING;
-const OIL = VISIBLE | LOW_DENSITY | GRAVITY | FLUID | LOW_VISCOSITY | FLAMMABLE;
-const FIRE = VISIBLE | LOW_DENSITY | FLY | LOW_VISCOSITY | VOLATILE | IGNITE | REACT_NEIGHBOURS | (FIRE_PLACE << PRODUCT_SHIFT);
-const PILLAR = VISIBLE | HIGH_DENSITY;
-const GAS = VISIBLE | LOW_DENSITY | FLY | LOW_VISCOSITY | FLAMMABLE | FLUID;
-const SEED = VISIBLE | HIGH_DENSITY | GRAVITY | HIGH_VISCOSITY | FLAMMABLE | NEED_SOIL | REACT_NEIGHBOURS | (TREE_PLACE << PRODUCT_SHIFT);
-const TREE = VISIBLE | HIGH_DENSITY | FLY | MEDIUM_VISCOSITY | TRAIL | VOLATILE | (WOOD_PLACE << PRODUCT_SHIFT);
-const WOOD = VISIBLE | HIGH_DENSITY | NEED_WATERING | REACT_ABOVE | FLAMMABLE | (TREE_PLACE << PRODUCT_SHIFT);
+const EMPTY = 0; // trees need to grow through empty so this might not be 0 later
+
+const SAND = HIGH_DENSITY | GRAVITY | LOW_SPREAD | FLAMMABLE | SOIL | NEED_WATERING | (MUD_PLACE << PRODUCT_SHIFT);
+const MUD = HIGH_DENSITY | GRAVITY | LOW_SPREAD | SOIL | SLOW;
+const WATER = MEDIUM_DENSITY | GRAVITY | FLUID | HIGH_SPREAD | WATERING;
+const OIL = LOW_DENSITY | GRAVITY | FLUID | HIGH_SPREAD | FLAMMABLE;
+const FIRE = LOW_DENSITY | FLY | HIGH_SPREAD | VOLATILE | IGNITE | REACT_NEIGHBOURS | (FIRE_PLACE << PRODUCT_SHIFT);
+const BLOCK = HIGH_DENSITY;
+const GAS = LOW_DENSITY | FLY | HIGH_SPREAD | FLAMMABLE | FLUID;
+const SEED = HIGH_DENSITY | GRAVITY | LOW_SPREAD | FLAMMABLE | NEED_SOIL | REACT_NEIGHBOURS | (TREE_PLACE << PRODUCT_SHIFT);
+const TREE = HIGH_DENSITY | FLY | MEDIUM_SPREAD | TRAIL | VOLATILE | (WOOD_PLACE << PRODUCT_SHIFT);
+const WOOD = HIGH_DENSITY | NEED_WATERING | REACT_ABOVE | FLAMMABLE | (TREE_PLACE << PRODUCT_SHIFT);
+const STONE = HIGH_DENSITY | GRAVITY | SLOW; // slow is set to have at least some vertical spread
 
 // declaring variables as local allows closure to simplify the names
 // remove these declarations after closure compiling
-var flags, pos, object, newPos, imgdata, update;
+var flags, pos, object, newPos, imgdata, update, md, mx, my, i, colour;
 var currentType = 0;
 var evenLoop = UPDATE_BIT;
 
 var field = new Uint32Array(SIZE); // the main field holding all information
 
-var particleTypes = [SAND, WATER, OIL, FIRE, PILLAR, GAS, SEED, // placable
+var particleTypes = [SAND, WATER, OIL, FIRE, BLOCK, GAS, SEED, STONE, SAND, SAND, // placable
     WOOD, TREE, MUD]; // not placable
 
 var colours = [];
-// aaBBGGRR
+// RGB
 colours[SAND|UPDATE_BIT] = 'ba8';//0x88aabb;
 colours[MUD|UPDATE_BIT] = '975';//0x7799bb;
 colours[WATER|UPDATE_BIT] = '27f';//0xff2222;
 colours[OIL|UPDATE_BIT] = '733';//0x333377;
 colours[FIRE|UPDATE_BIT] = 'f00';//0x0000ff;
-colours[PILLAR| UPDATE_BIT] = '888';//0x888888;
+colours[BLOCK| UPDATE_BIT] = '888';//0x888888;
 colours[GAS| UPDATE_BIT] = '060';//0x006600;
 colours[TREE| UPDATE_BIT] = '0a0';//0x00aa00;
 colours[SEED | UPDATE_BIT] = '6a6';//0x66aa66;
 colours[WOOD | UPDATE_BIT] = '980';//0x008899;
+colours[STONE | UPDATE_BIT] = 'bbb';
+
+// doing this calcualtion each step was terrible for performance
+// therefore, calculate the colours now
+var pixelColours = [];
+for (colour in colours){
+    for (i=3;i--;){
+        pixelColours[colour] |= "0x"+colours[colour][i]+colours[colour][i]<<i*8;
+    }
+};
 
 var drawData = new ArrayBuffer(SIZE*4);
 var pixel32Array = new Uint32Array(drawData)
 var imgDataArray = new Uint8ClampedArray(drawData);
 
-// for (i=HEIGHT;i--;){
-    
-    // make vertical wall
-    // field[i*WIDTH]=HIGH_DENSITY;
-    // the floor can be done with less bytes in the move conditions
-    // if there is only 1 moving down condition
-// }
-
-
-// when the mouse is pressed, create 20 objects under the cursor
-// onmousedown = e => {
-//     for (i=DROP_SIZE;i--;){
-//         field[e.offsetX+e.offsetY*WIDTH+i] =
-//         field[e.offsetX+e.offsetY*WIDTH-i] =
-//         field[e.offsetX+(e.offsetY+i)*WIDTH] =
-//         field[e.offsetX+(e.offsetY-i)*WIDTH] = particleTypes[currentType%NUM_PLACABLE_TYPES];
-//     }
-// }
-var md = 0
-var mx = my = -1
-onmousedown = e => ++md
-onmouseup   = e => --md
-onmousemove = e => (mx = e.offsetX) && (my = e.offsetY)
+md = 0;
+mx = my = -1;
+// right clicking used to give a problem because it sometimes triggers mousedown but not mouseup
+// setting instead of adding should fix this
+onmousedown = e => md=1;
+onmouseup   = e => md=0;
+onmousemove = e => {mx = e.offsetX; my = e.offsetY};
 
 
 
 
-// press any key to change type of spawned particle
-onkeydown = e => ++currentType
+// press the number keys to set the corresponding type of spawned particle
+// any other key gets the first particle (sand)
+onkeydown = e => currentType = e.key|0
 
 // var startTime = Date.now();
 // var totalSteps = 0;
+// var updateEnd = Date.now();
+
 update = e => {
-    // for (i=DROP_SIZE;md && i--;){
-    //     field[mx+my*WIDTH+i] =
-    //     field[mx+my*WIDTH-i] =
-    //     field[mx+(my+i)*WIDTH] =
-    //     field[mx+(my-i)*WIDTH] = particleTypes[currentType%NUM_PLACABLE_TYPES];
-    // }
-    for (y=9;md && --y>-9;)
-        for (x=9;--x>-9;)
-            if (x*x+y*y < 81)
-                field[mx+x+(my+y)*WIDTH] = particleTypes[currentType%NUM_PLACABLE_TYPES] ^ evenLoop ^ UPDATE_BIT
     
-    // var updateStart = Date.now();
+//     var updateStart = Date.now();
+    
+    for (y=DROP_SIZE;md && --y>-DROP_SIZE;){
+        for (x=DROP_SIZE;--x>-DROP_SIZE;){
+            if (x*x+y*y < DROP_SIZE*DROP_SIZE){
+                field[mx+x+(my+y)*WIDTH] = particleTypes[currentType%NUM_PLACABLE_TYPES] ^ evenLoop ^ UPDATE_BIT;
+            }
+        }
+    }
     
     pixel32Array.fill(0xff000000);
     
     for (pos=SIZE; pos--;){
         flags = field[pos];
-        if (flags & VISIBLE && UPDATE_BIT & flags ^ evenLoop){
+        if (flags ^ EMPTY && UPDATE_BIT & flags ^ evenLoop){
             
             // the drawing is one frame behind on the physics, but I don't think that matters
             // this is way simpler
-            // pixel32Array[pos] |= colours[flags|UPDATE_BIT];
-            for (i=3;i--;)
-                imgDataArray[pos*4+i]="0x"+colours[flags|UPDATE_BIT][i]+colours[flags|UPDATE_BIT][i]|0;
+            pixel32Array[pos] |= pixelColours[flags|UPDATE_BIT];
             
             if (flags & VOLATILE && Math.random() < .1){
                 flags = 0;
@@ -181,10 +178,10 @@ update = e => {
             }
             
             
-            // most probably go straight down, but there is a small chance to go left or right
+            // most probably go straight down, but there is a small chance to go left or right depending on spread
             newPos = pos +
-                WIDTH*(!!(flags & GRAVITY) - !!(flags & FLY)) * (!(flags&SLOW) || Math.random()>.5) +
-                (Math.random() < (flags & VISCOSITY)*1.3)*(Math.random()>.5?1:-1);
+                WIDTH*(!!(flags & GRAVITY) - (flags&SLOW && Math.random()>.5)) +
+                ((Math.random() - .5) * (1+(flags & SPREAD))|0);
             
             
             // if the newPos is not solid, or this cell is fluid and the cell above newPos is not solid
@@ -224,7 +221,8 @@ update = e => {
     // toggle update bit of update
     evenLoop ^= UPDATE_BIT;
     
-    // console.log((Date.now()-updateStart));//-startTime)/++totalSteps);;
+//     console.log((Date.now()-updateEnd));/*/-startTime)/++totalSteps);/**/
+//     updateEnd = Date.now();
     
     requestAnimationFrame(update);
 }//,DELAY);
